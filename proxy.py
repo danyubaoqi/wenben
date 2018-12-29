6f3db692d9b3f845858afd399732b4cbe6a1eb92
puring <784932562@qq.com>
2018-12-29 15:19:52 +0800

    123

--- proxy.py
+++ proxy.py
@@ -3,9 +3,9 @@ import json
 class Proxy:
     def get_proxy(self):
 
-        a=requests.get("http://54.190.145.159:8000/").text
+        a=requests.get("http://54.245.156.160:8000/").text
         data=json.loads(a)
         return data
 if __name__ == '__main__':
     a=Proxy()
-    print(a.get_proxy())
+    print(len(a.get_proxy()))
--- test.py
+++ test.py
@@ -13,8 +13,8 @@ from pymysql import connect
 from sqlite3 import connect as sqc
 
 # pool = PooledDB(pymysql, 10, host="127.0.0.1", user="test", passwd="", db="wenben_book", port=8889)
-# con = connect("localhost", "test", "", "wenben_book", 8889)
-con3 = sqc("book.db")
+con3 = connect("localhost", "root", "", "wenben_book", 3306)
+# con3 = sqc("book.db")
 proxy = Proxy()
 try:
     con3.cursor().execute(
@@ -33,6 +33,10 @@ def randList(ls: list):
     return random.choice(ls)
 
 
+def delete_proxy(ip):
+    proxy.delete_proxy(ip)
+
+
 #
 
 
@@ -45,6 +49,7 @@ def generate_cookie():
 
 
 def exesql(con: con3, sql: str):
+    con.ping(reconnect=True)
     cur = con.cursor()
     cur.execute(sql)
     cur.close()
@@ -56,23 +61,50 @@ if __name__ == '__main__':
     proxies = get_proxy()
 
 
-    @threads(10)
+    @threads(15)
     def get_bs(url: str, headers: dict, cookie: dict):
-        bs = BeautifulSoup(requests.get(url, cookies=cookie, headers=headers).text, "html.parser")
+        rp = random.choice(get_proxy())
+        pro = {"http": f"http://{rp[0]}:{rp[1]}", "https": f"http://{rp[0]}:{rp[1]}"}
+        p=5
+        while p>0:
+            try:
+                delete_proxy(rp[0])
+                rp = random.choice(get_proxy())
+                pro = {"http": f"http://{rp[0]}:{rp[1]}", "https": f"http://{rp[0]}:{rp[1]}"}
+                bs = BeautifulSoup(requests.get(url, cookies=cookie, headers=headers,proxies=pro).text, "html.parser")
+                break
+            except:
+                p-=1
+                continue
+
         p = bs.find_all("script", type="application/ld+json")
         data = p[0].get_text()
-
+        data = data.decode("utf-8")
         ul = json.loads(data, strict=False)["url"]
-        exesql(con3, f"insert into test values ('{ul}','{data}')")
+        exesql(con3, f"insert into movie values ('{ul}','{data}')")
 
 
     for page in range(1, 10000):
-        proxies = get_proxy()
-        print(f"开始第{page}页")
-        url = f"https://movie.douban.com/j/new_search_subjects?sort=U&range=0,10&tags=&start={page * 20}&year_range=2017,2017"
-        a = requests.get(url, cookies=generate_cookie(), headers=generate_headers(), proxies=random.sample(proxies))
+        try:
+            rp = random.choice(get_proxy())
+            pro = {"http": f"http://{rp[0]}:{rp[1]}", "https": f"http://{rp[0]}:{rp[1]}"}
+            print(f"开始第{page}页")
+            url = f"https://movie.douban.com/j/new_search_subjects?sort=U&range=0,10&tags=&start={page * 20}&year_range=2017,2017"
 
-        print(a.text)
+            a = requests.get(url, cookies=generate_cookie(), headers=generate_headers(), proxies=pro)
+        except:
+            p = 0
+            while p < 3:
+                try:
+                    delete_proxy(rp[0])
+                    rp = random.choice(get_proxy())
+                    pro = {"http": f"http://{rp[0]}:{rp[1]}", "https": f"http://{rp[0]}:{rp[1]}"}
+                    a = requests.get(url, cookies=generate_cookie(), headers=generate_headers(), proxies=pro)
+                    break
+                except:
+                    p+=1
+                    continue
+            continue
         data = json.loads(a.text)
         for url in data["data"]:
             headers = {"user-agent": user_agent.generate_user_agent()}
